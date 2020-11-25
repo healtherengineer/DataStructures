3ad170de6a2dba3e2c57a7293f52fcabbd36f887
Ramazan Kahraman <ege.barisan.01@gmail.com>
2020-11-25 10:20:09 +0300

    BinarySearch added 25.11.2020

---
+++ BinarySearch/src/Main.java
@@ -0,0 +1,36 @@
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static int binarySearch(int[] arr, int l, int m, int r, int x) {
+
+        if (arr[m] == x) {
+            return m;
+        } else if (l == r) {
+            return -1;
+
+        }
+        if (arr[m] > x) {
+            r = m - 1;
+
+        } else {
+            l = m + 1;
+        }
+        m = (l + r) / 2;
+        return binarySearch(arr, l, m, r, x);
+
+    }
+
+    public static void main(String[] args) {
+
+        // assuming the array as sorted or enter a sorted array directly
+        int[] array = {11, 22, 55, 88, 121, 122, 132};
+        System.out.println(Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+        System.out.println("index of element is = " + binarySearch(array, 0, ((array.length - 1) / 2), array.length - 1, 22));
+
+    }
+
+}
